d7998beff3d59016f8ffdc0f4115f4f2dd3748ff
Arpad Bozzay <arpad.bozzay@xtendr.io>
2020-03-10 12:42:04 +0100

    basic routing

--- infinitum/src/index.js
+++ infinitum/src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
+import App from './App'
+import Registration from './pages/Registration';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <ul>
+                <li>
+                    <Link to="/registration">Registration</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+            </ul>
+            {/* ProtectedRoutes */}
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/registration" component={Registration} />
+                <Route path="/login" component={Login} />
+                <Route path="/home" component={Home} />
+                <Route component={NotFound} />
+            </Switch>
+        </div>
+    </Router>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(routing, document.getElementById('root'));
---
+++ infinitum/src/pages/Registration.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+class Registration extends React.Component {
+    render() {
+        return <h1>Registration</h1>
+    }
+}
+export default Registration;
